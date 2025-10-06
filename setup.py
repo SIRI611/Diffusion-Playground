e13cf229db205a1ec31722c92e8ff5b0ab6d9d5a
Xirui (Dalen) Shi <dalen.shi@outlook.com>
2025-10-06 10:34:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="diffusion",                          # Name of your package
-    version="0.1.0",                            # Version
-    author="Dalen Shi",                         # Author name
+    name="diffusion",                         
+    version="0.1.0",                            
+    author="Dalen Shi",                         
     description="A framwork for diffuison models",
     long_description=open("README.md").read(),  # Long description (README)
     long_description_content_type="text/markdown",
-    # url="https://github.com/username/my_project",  # Project URL
-    packages=find_packages(),                   # Automatically find packages
+    packages=find_packages(),                 
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",                    # Minimum Python version
+    python_requires=">=3.7",                  
     install_requires=[
-        "numpy",                                # List dependencies here
+        "numpy",                                
         "torch",
     ],
 )
